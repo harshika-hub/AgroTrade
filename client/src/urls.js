8c2ead69c3dfb3abb568f70075b49f267d22fbc8
Hemant Prajapati <hemantpraja88@gmail.com>
2023-12-17 00:57:35 +0530

    updated 17/12/23 00:55

--- client/src/urls.js
+++
@@ -1,4 +0,0 @@
-export const REQUESTED_URL = "http://localhost:3000"
-export const USER_REQUESTED_URL = "http://localhost:3000/user"
-export const ORGANIZATION_REQUESTED_URL = "http://localhost:3000/organization"
-export const ADMIN_REQUESTED_URL = "http://localhost:3000/admin"
