528991ca882b8450230eb9264ebefeef24533d74
Harshika <harshika@gmail.com>
2024-02-12 14:20:30 +0530

    video call render deploy

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 app.use(cors());
 
 // const httpServer=http.createServer(app);
